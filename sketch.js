7444d506c27189ab870bd616534b1bc952510768
Shubham Meshram <meshram.shubham27@gmail.com>
2023-01-23 23:55:43 +0530

    general function for creating lines

--- sketch.js
+++ sketch.js
@@ -1,21 +1,31 @@
 const CRYSTAL_SIZE = 500;
 const SIDES = 6;
+let PALETTE = [];
 
 function setup() {
   createCanvas(550, 550, SVG);
   noLoop();
   angleMode(DEGREES);
   rectMode(CENTER);
+
+  PALETTE = [color(255, 52, 154), color(4, 0, 152)];
 }
 
 function draw() {
-  noFill(0);
-  stroke(0);
+  testLines();
+}
+
+function testLines() {
+  noFill();
   push();
-  translate(width / 2, height / 2);
-  ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
-  for (i = 0; i < 6; i++) {
-    line(0, 0, 0, CRYSTAL_SIZE / 2);
-  }
+    translate(width / 2, height / 2);
+    stroke(PALETTE[0]);
+    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
+    stroke(PALETTE[1]);
+    const angle = 360 / SIDES;
+    for (let i = 0; i < SIDES; i++) {
+        line(0, 0, 0, CRYSTAL_SIZE / 2);
+        rotate(angle);
+    }
   pop();
 }
