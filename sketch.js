af7a06689fea60c77ac96b80ddf50adaef770caf
Shubham Meshram <meshram.shubham27@gmail.com>
2023-01-29 23:45:19 +0530

    added randomizer function for shapes

--- sketch.js
+++ sketch.js
@@ -8,7 +8,10 @@ function setup() {
   angleMode(DEGREES);
   rectMode(CENTER);
 
-  PALETTE = [color(255, 52, 154), color(4, 0, 152)];
+  PALETTE = [
+    color(255, 52, 154),
+    color(4, 0, 152)
+  ];
 }
 
 function draw() {
@@ -16,16 +19,35 @@ function draw() {
 }
 
 function testLines() {
+  let numShapes = randomizer() ? SIDES : SIDES * 2;
+  const strokeColor = getRandomPalette();
+ 
   noFill();
   push();
     translate(width / 2, height / 2);
     stroke(PALETTE[0]);
     ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
-    stroke(PALETTE[1]);
-    const angle = 360 / SIDES;
-    for (let i = 0; i < SIDES; i++) {
+
+    stroke(strokeColor);
+    const angle = 360 / numShapes;
+    for (let i = 0; i < numShapes; i++) {
         line(0, 0, 0, CRYSTAL_SIZE / 2);
         rotate(angle);
     }
   pop();
+}
+
+function randomizer() {
+  const rando = random(1);
+  if (rando > 0.5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getRandomPalette() {
+  const randoTwo = floor(random(0, PALETTE.length));
+  return PALETTE[randoTwo];
+
 }
