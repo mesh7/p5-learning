2455b5adb3194cc92b425b8c5dbe42d5307ed4e3
Shubham Meshram <meshram.shubham27@gmail.com>
2023-02-07 23:29:41 +0530

    added function for connecting lines

--- sketch.js
+++ sketch.js
@@ -1,53 +1,88 @@
-const CRYSTAL_SIZE = 500;
-const SIDES = 6;
-let PALETTE = [];
+// create outlineShape function
+// draw ellipse
+// add random weight and color
+// add hexagon
+
+const CRYSTAL_SIZE = 500
+const SIDES = 6
+let PALETTE = []
 
 function setup() {
-  createCanvas(550, 550, SVG);
-  noLoop();
-  angleMode(DEGREES);
-  rectMode(CENTER);
+  createCanvas(530, 530, SVG)
 
   PALETTE = [
-    color(255, 52, 154),
-    color(4, 0, 152)
-  ];
+    color(255, 52, 154), // pink
+    color(4, 0, 152) // blue
+  ]
+
+  noLoop()
+  angleMode(DEGREES)
+  rectMode(CENTER)
 }
 
 function draw() {
-  testLines();
+  // testLines()
+  // outlineShape()
+  simpleLines()
 }
 
-function testLines() {
-  let numShapes = randomizer() ? SIDES : SIDES * 2;
-  const strokeColor = getRandomPalette();
- 
-  noFill();
-  push();
-    translate(width / 2, height / 2);
-    stroke(PALETTE[0]);
-    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE);
+function simpleLines() {
+  const stepsOut = 8;
+  const numSteps = randomSelectTwo ? stepsOut : int(stepsOut * 1.25)
+  const step = (CRYSTAL_SIZE / 2 ) / numShapes
+  const start = floor(random(0, numsteps))
+  const start = floor(random(0, numsteps))
 
-    stroke(strokeColor);
-    const angle = 360 / numShapes;
+  let numShapes = randomSelectTwo() ? SIDES : SIDES * 2
+  const strokeColor = getRandomFromPalette()
+  const weight = randomSelectTwo() ? 1 : 3
+
+  const angle = 360 / numShapes
+
+  noFill()
+  stroke(strokeColor)
+  strokeWeight(weight)
+  push()
+    translate(width/2, height/2)
     for (let i = 0; i < numShapes; i++) {
-        line(0, 0, 0, CRYSTAL_SIZE / 2);
-        rotate(angle);
+      line(0, 0, 0, CRYSTAL_SIZE / 2)  
+      rotate(angle)
     }
-  pop();
+  pop()
 }
 
-function randomizer() {
-  const rando = random(1);
-  if (rando > 0.5) {
-    return true;
+function outlineShape () {
+  const strokeColor = getRandomFromPalette()
+  const weight = randomSelectTwo() ? 1 : 3
+  const hexagonTrue = randomSelectTwo()
+
+  stroke(strokeColor)
+  strokeWeight(weight)
+  push()
+  translate(width/2, height/2)
+  if (hexagonTrue) {
+    hexagon(0, 0, CRYSTAL_SIZE / 2)
   } else {
-    return false;
+    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
   }
+  pop()
 }
 
-function getRandomPalette() {
-  const randoTwo = floor(random(0, PALETTE.length));
-  return PALETTE[randoTwo];
+function testLines () {
+  let numShapes = randomSelectTwo() ? SIDES : SIDES * 2
+  const strokeColor = getRandomFromPalette()
 
+  noFill()
+  stroke(PALETTE[0])
+  push()
+    translate(width/2, height/2)
+    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
+    
+    stroke(strokeColor)
+    const angle = 360 / numShapes
+    for (let i = 0; i < numShapes; i++) {
+      line(0, 0, 0, CRYSTAL_SIZE / 2)  
+      rotate(angle)
+    }
+  pop()
 }
